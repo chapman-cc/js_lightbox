2afd08983c42726425576ecc5ec19431e6e5bebf
chapmanCC <chapboy1989@gmail.com>
2019-01-29 00:07:11 +0800

    rename var $imgCaptions[0].imgName to imgAlt

--- js/main.js
+++ js/main.js
@@ -2,76 +2,75 @@ $(document).ready(function() {
     // ========================================
     // ---------- Variables ----------
 
-    const imgCaptions = [
-        {
+    const $imgCaptions = [{
             imgFile: '01',
-            imgName: 'Hay Bales',
+            imgAlt: 'Hay Bales',
             imgCaption: 'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
         },
 
         {
             imgFile: '02',
-            imgName: 'Lake',
+            imgAlt: 'Lake',
             imgCaption: 'The lake was so calm today.We had a great view of the snow on the mountains from here.'
         },
 
         {
             imgFile: '03',
-            imgName: 'Canyon',
+            imgAlt: 'Canyon',
             imgCaption: 'I hiked to the top of the mountain and got this picture of the canyon and trees below.'
         },
 
         {
             imgFile: '04',
-            imgName: 'Iceberg',
+            imgAlt: 'Iceberg',
             imgCaption: 'It was amazing to see an iceberg up close, it was so cold but didn’ t snow today.'
         },
 
         {
             imgFile: '05',
-            imgName: 'Desert',
+            imgAlt: 'Desert',
             imgCaption: 'The red cliffs were beautiful.It was really hot in the desert but we did a lot of walking through the canyons.'
         },
 
         {
             imgFile: '06',
-            imgName: 'Fall',
+            imgAlt: 'Fall',
             imgCaption: 'Fall is coming, I love when the leaves on the trees start to change color.'
         },
 
         {
             imgFile: '07',
-            imgName: 'Plantation',
+            imgAlt: 'Plantation',
             imgCaption: 'I drove past this plantation yesterday, everything is so green!'
         },
 
         {
             imgFile: '08',
-            imgName: 'Dunes',
+            imgAlt: 'Dunes',
             imgCaption: 'My summer vacation to the Oregon Coast.I love the sandy dunes!'
         },
 
         {
-            imgFile: '    09',
-            imgName: 'Countryside Lane',
+            imgFile: '09',
+            imgAlt: 'Countryside Lane',
             imgCaption: 'We enjoyed a quiet stroll down this countryside lane.'
         },
 
         {
             imgFile: '10',
-            imgName: 'Sunset',
+            imgAlt: 'Sunset',
             imgCaption: 'Sunset at the coast!The sky turned a lovely shade of orange.'
         },
 
         {
             imgFile: '11',
-            imgName: 'Cave',
+            imgAlt: 'Cave',
             imgCaption: 'I did a tour of a cave today and the view of the landscape below was breathtaking.'
         },
 
         {
             imgFile: '12',
-            imgName: 'Bluebells',
+            imgAlt: 'Bluebells',
             imgCaption: 'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in .'
         }
     ];
